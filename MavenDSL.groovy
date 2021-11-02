8121625308af457444dd82ae9af6851e4cb52b5f
dayanka@NLRTM1-L0601 <karunakaran.dayanithi@navis.com>
2021-11-02 11:29:29 +0100

    DSL Groovy Updated

--- MavenDSL.groovy
+++ MavenDSL.groovy
@@ -1,7 +1,7 @@
 job('Maven_DSL_JOB') {
     logRotator(3, 3)
     scm {
-        github('https://github.com/karunakaran1983/Maven_Single_Module.git', 'master')
+        git('https://github.com/karunakaran1983/Maven_Single_Module.git', 'master')
     }
     triggers {
         scm('* * * * *')
